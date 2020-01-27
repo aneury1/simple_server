1b428cdae70195bb5f44938b8b7f2454c37fbde2
aneury <aneury1@gmail.com>
2020-01-27 11:03:57 -0400

    refactoring buid system

---
+++ build/CMakeFiles/AHTTPServer.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AHTTPServer.dir/src/HTTPPath.cpp.o"
+  "CMakeFiles/AHTTPServer.dir/src/HttpRequest.cpp.o"
+  "CMakeFiles/AHTTPServer.dir/src/JSON.cpp.o"
+  "CMakeFiles/AHTTPServer.dir/src/Server.cpp.o"
+  "CMakeFiles/AHTTPServer.dir/src/main.cpp.o"
+  "AHTTPServer.pdb"
+  "AHTTPServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AHTTPServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
